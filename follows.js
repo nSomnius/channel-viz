5b26854ddd07f34ea5efb8086ffede85d228495d
nSomnius <Sunstarer@gmail.com>
2014-08-02 13:33:55 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,10 +14,10 @@
 
 	var defaultKey		= '2FzHEDpMWVCTpWgTadbI5TidT0xOPNNrVRRaFW5SuRh79wjw', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1032716905], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+		applicationName	= 'Temperature and Humidity', // Replaces Xively logo in the header
 		dataDuration	= '3days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= '000', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
